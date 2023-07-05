ab7cfe7d6d0a788217bd5818b30e2a90515c4f15
MasoudKhz <87560735+MasoudKhz@users.noreply.github.com>
2023-07-05 13:01:41 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -116,7 +116,7 @@ install_x-ui() {
             exit 1
         fi
         echo -e "Got x-ui latest version: ${last_version}, beginning the installation..."
-        wget -N --no-check-certificate -O /usr/local/x-ui-linux-$(arch3xui).tar.gz https://github.com/MasoudKhz/3x-ui/releases/${last_version}/x-ui-linux-$(arch3xui).tar.gz
+        wget -N --no-check-certificate -O /usr/local/x-ui-linux-$(arch3xui).tar.gz https://github.com/MasoudKhz/3x-ui/archive/refs/tags/p2.tar.gz
         if [[ $? -ne 0 ]]; then
             echo -e "${red}Downloading x-ui failed, please be sure that your server can access Github ${plain}"
             exit 1
